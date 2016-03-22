6392e3dbdc083feca76cbd5a82f496970dc03ae1
Alan Souza <alansouzati@gmail.com>
2016-03-22 01:11:07 -0700

    Reducing overhead for ios devices for slide animation

--- src/js/components/Slides.js
+++ src/js/components/Slides.js
@@ -102,7 +102,7 @@ export default class Slides extends Component {
     return (
       <Box direction='row' className={CLASS_ROOT}>
         <Motion key={this.state.activeIndex}
-          defaultStyle={{x: 20}} style={{x: spring(0)}}>
+          defaultStyle={{x: 10}} style={{x: spring(0)}}>
           {({x}) =>
             <div style={{
               WebkitTransform: `translate3d(${x}%, 0, 0)`,
